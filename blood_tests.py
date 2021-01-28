92ed6848cf238ad6b055fa8ce57add5b010df66c
Haley Snyder <hes22@duke.edu>
2021-01-28 16:29:21 -0500

    modified interface function and created HDL input function

--- blood_tests.py
+++ blood_tests.py
@@ -7,5 +7,19 @@ def interface():
         if choice == "9" :
             return
 
-interface()
+def HDL_driver():
+    #get data
+    print("Check HDL")
+    level = input("Enter HDL level: ")
+    level = int(level)
+    #analyze data
+    if level < 40 :
+        print("Low")
+    elif level >= 40 | level < 60 :
+        print("Borderline Low")
+    else :
+        print("High")
+    #output data
+
+HDL_driver()
 
