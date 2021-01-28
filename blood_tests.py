c5ead4e1e55b6362cfbf99a89f775277cc554724
Haley Snyder <hes22@duke.edu>
2021-01-28 17:01:54 -0500

    create total cholesterol driver function to use future functions

--- blood_tests.py
+++ blood_tests.py
@@ -71,5 +71,33 @@ def output_LDL(LDL, analysis):
     print("The level is {}".format(analysis))
     #output data
 
+def tc_driver():
+    tc = get_tc_input()
+    analysis = analyze_tc(tc)
+    output_tc(tc,analysis)
+    
+    
+def get_HDL_input():
+    #get data
+    print("Check HDL")
+    level = input("Enter HDL level: ")
+    level = int(level)
+    return level
+
+def analyze_HDL(level):
+    #analyze data
+    if level < 40 :
+        return "Low"
+    elif 40 <= level < 60 :
+        return "Borderline Low"
+    else :
+        return "High"
+
+def output_HDL(HDL, analysis):
+    print("The HDL entered was {}".format(HDL))
+    print("The level is {}".format(analysis))
+    #output data
+
+
 interface()
 
