4a09c305d4f1d827991c706be927d618688435da
Haley Snyder <hes22@duke.edu>
2021-01-28 16:51:17 -0500

    create LDL driver function to eventually call future functions

--- blood_tests.py
+++ blood_tests.py
@@ -40,5 +40,31 @@ def output_HDL(HDL, analysis):
     print("The level is {}".format(analysis))
     #output data
 
+def LDL_driver():
+    LDL = get_LDL_input()
+    analysis = analyze_LDL(LDL)
+    output_LDL(LDL,analysis)
+
+def get_HDL_input():
+    #get data
+    print("Check HDL")
+    level = input("Enter HDL level: ")
+    level = int(level)
+    return level
+
+def analyze_HDL(level):
+    #analyze data
+    if level < 40 :
+        return "Low"
+    elif 40 <= level < 60 :
+        return "Borderline Low"
+    else :
+        return "High"
+
+def output_HDL(HDL, analysis):
+    print("The HDL entered was {}".format(HDL))
+    print("The level is {}".format(analysis))
+    #output data
+
 interface()
 
