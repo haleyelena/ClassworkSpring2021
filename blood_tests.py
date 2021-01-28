19722a1d5d5df30ce9ac339b3c43e844eb9d2b5a
Haley Snyder <hes22@duke.edu>
2021-01-28 16:32:44 -0500

    modified HDL analyze and HDL output functions (I forgot to commit these seperately)

--- blood_tests.py
+++ blood_tests.py
@@ -2,24 +2,38 @@ def interface():
     print("Blood Test Analysis")
     while True:
         print("\nOptions")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Enter an option: ")
         if choice == "9" :
             return
+        elif choice == "1":
+            HDL_driver()
 
 def HDL_driver():
+    
+    
+    
+def get_HDL_input():
     #get data
     print("Check HDL")
     level = input("Enter HDL level: ")
     level = int(level)
+    return level
+
+def analyze_HDL():
     #analyze data
     if level < 40 :
         print("Low")
-    elif level >= 40 | level < 60 :
+    elif 40 <= level < 60 :
         print("Borderline Low")
     else :
         print("High")
+
+def output_HDL(HDL, analysis):
+    print("The HDL entered was {}".format(HDL))
+    print("The level is {}".format(analysis))
     #output data
 
-HDL_driver()
+interface()
 
