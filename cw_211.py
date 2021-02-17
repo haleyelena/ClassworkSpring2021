676aa6a7a1dbcaa6e7c60071332a4b4ed67a5a19
Haley Snyder <hes22@duke.edu>
2021-02-16 22:03:22 -0500

    extra work for classwork 2/11

--- cw_211.py
+++ cw_211.py
@@ -3,3 +3,12 @@ def point_finder(p1, p2, x):
     slope = (p2[1]-p1[1])/(p2[0]-p1[0])
     y = slope*(x-p2[0]) + p2[1]
     return y
+
+#additional work
+def point_check(p1, p2, p3):
+    slope = (p2[1]-p1[1])/(p2[0]-p1[0])
+    y = slope*(p3[0]-p2[0]) + p2[1]
+    if y == p3[1]:
+        return True
+    else:
+        return False
--- test_cw_211.py
+++ test_cw_211.py
@@ -9,3 +9,14 @@ def test_point_finder(p1, p2, x, expected):
     from cw_211 import point_finder
     answer = point_finder(p1, p2, x)
     assert answer == expected
+
+
+@pytest.mark.parametrize("p1, p2, p3, expected", [
+    ((1, 2), (3, 4), (2, 3), True),
+    ((1, 5), (5, 9), (3, 7), True),
+    ((1, 5), (5, 9), (3, 9), False)
+])
+def test_point_check(p1, p2, p3, expected):
+    from cw_211 import point_check
+    answer = point_check(p1, p2, p3)
+    assert answer == expected
